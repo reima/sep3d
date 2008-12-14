8a963dcf4598106b73c3822b1b58cf47e8db58f3
unknown <hager@.domain-i15.local>
2008-12-14 23:52:41 +0100

    DynamicLODSelector

---
+++ TerrainRenderer/DynamicLODSelector.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cmath>
+#include "DynamicLODSelector.h"
+#include "Tile.h"
+
+#undef min
+#undef max
+
+DynamicLODSelector::DynamicLODSelector(float fov_y,
+                                       int screen_height,
+                                       float max_error) {
+  factor_ = 2 * std::tan(fov_y / 2) * max_error / (float)screen_height;
+}
+
+DynamicLODSelector::~DynamicLODSelector(void) {
+}
+
+bool DynamicLODSelector::IsLODSufficient(const Tile *tile,
+                                         const CBaseCamera *camera) const {
+  D3DXVECTOR3 bbox[8];
+  tile->GetBoundingBox(bbox, NULL);
+  D3DXVec3TransformCoordArray(bbox, sizeof(D3DXVECTOR3),
+                              bbox, sizeof(D3DXVECTOR3),
+                              camera->GetViewMatrix(), 8);
+  float max_z = bbox[0].z;
+  for (int i = 1; i < 8; ++i) max_z = std::max(max_z, bbox[i].z);
+  float max_world_error = factor_ * max_z;
+  return tile->GetWorldError() <= max_world_error;
+}
---
+++ TerrainRenderer/DynamicLODSelector.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "LODSelector.h"
+
+class DynamicLODSelector : public LODSelector {
+ public:
+  DynamicLODSelector(float fov_y, int screen_height, float max_error);
+  ~DynamicLODSelector(void);
+
+  virtual bool IsLODSufficient(const Tile *tile,
+                               const CBaseCamera *camera) const;
+
+ private:
+  /**
+   * delta = factor_ * tau * z;
+   * delta: World Space Error
+   * tau: Auf View Plan proj. World Space Error
+   */
+  float factor_;
+};
